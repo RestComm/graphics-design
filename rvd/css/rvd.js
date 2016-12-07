7d3084b2ccac2e9fa51f362bcf331fab6a677eb9
Despina Ipsilanti <despina.ipsilanti@telestax.com>
2016-12-07 17:51:32 +0200

    Improvements and additions

---
+++ rvd/css/rvd.js
@@ -0,0 +1,19 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+$(function()
+  {
+      var f=function()
+      {
+          $(this).next().text($(this).is(':checked') ? ':checked' : ':not(:checked)');
+      };
+      $('input').change(f).trigger('change');
+  });
